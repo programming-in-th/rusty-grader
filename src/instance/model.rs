f2cb6e1a5cd45954ded0404e96315b45509940b6
win11905 <donothavemoney@gmail.com>
2021-09-13 19:17:15 -0400

    Refactor

--- src/instance/model.rs
+++ src/instance/model.rs
@@ -1,5 +1,10 @@
+macro_rules! s {
+    ($x:expr) => {
+        String::from($x);
+    };
+}
 use std::env;
-use std::path::{ Path, PathBuf };
+use std::path::{Path, PathBuf};
 use std::process::Command;
 
 use crate::instance::error::InstanceError;
@@ -62,25 +67,25 @@ impl Instance {
 
     fn get_arguments(&self) -> Result<Vec<String>, InstanceError> {
         let mut args: Vec<String> = Vec::new();
-        args.push("--cg".to_string());
-        args.push("--cg-timing".to_string());
-        args.push("--processes=128".to_string());
+        args.push(s!("--cg"));
+        args.push(s!("--cg-timing"));
+        args.push(s!("--processes=128"));
 
-        args.push("-b".to_string());
+        args.push(s!("-b"));
         args.push(self.box_id.to_string());
 
-        args.push("-M".to_string());
+        args.push(s!("-M"));
         args.push(self.log_file.to_str().unwrap().to_string());
 
-        args.push("-t".to_string());
+        args.push(s!("-t"));
         args.push(self.time_limit.to_string());
 
         args.push(format!("--cg-mem={}", self.memory_limit));
 
-        args.push("-w".to_string());
+        args.push(s!("-w"));
         args.push((self.time_limit + 5.0).to_string());
 
-        args.push("-x".to_string());
+        args.push(s!("-x"));
         args.push((self.time_limit + 1.0).to_string());
 
         let alternative_path = get_env("ALTERNATIVE_PATH")?;
@@ -89,16 +94,16 @@ impl Instance {
             args.push(format!("--dir={}", alternative_path));
         }
 
-        args.push("-i".to_string());
+        args.push(s!("-i"));
         args.push(self.input_path.to_str().unwrap().to_string());
 
-        args.push("-o".to_string());
+        args.push(s!("-i"));
         args.push(self.output_path.to_str().unwrap().to_string());
 
         Ok(args)
     }
 
-    fn check_root_permission() -> Result<(), InstanceError> {
+    fn check_root_permission(&self) -> Result<(), InstanceError> {
         let permission_result = Command::new("id").arg("-u").output();
         match permission_result {
             Ok(output) => {
@@ -106,15 +111,19 @@ impl Instance {
                 if output_string.trim() == "0" {
                     Ok(())
                 } else {
-                    Err(InstanceError::PermissionError(String::from("isolate must be run as root")))
+                    Err(InstanceError::PermissionError(s!(
+                        "isolate must be run as root"
+                    )))
                 }
-            },
-            _ => Err(InstanceError::PermissionError(String::from("unable to get current user id")))
+            }
+            _ => Err(InstanceError::PermissionError(s!(
+                "unable to get current user id"
+            ))),
         }
     }
 
     pub fn init(&mut self) -> Result<(), InstanceError> {
-        // self.check_root_permission()?;
+        self.check_root_permission()?;
 
         let isolate_path = get_env("ISOLATE_PATH")?;
 
@@ -124,7 +133,7 @@ impl Instance {
             .arg(self.box_id.to_string())
             .arg("--init")
             .output().map_err(|_|InstanceError::PermissionError(
-                String::from("Unable to run isolate --init command. Does a box already exist? If so, you must clean up first.")
+                s!("Unable to run isolate --init command. Does a box already exist? If so, you must clean up first.")
             ))?;
 
         self.box_path = PathBuf::from(String::from_utf8(box_path.stdout).unwrap()).join("box");
@@ -133,29 +142,33 @@ impl Instance {
             .arg(self.input_path.to_str().unwrap())
             .arg(self.box_path.join("input").to_str().unwrap())
             .output()
-            .map_err(|_| InstanceError::PermissionError(
-                String::from("Unable to copy input file into box directory")
-            ))?;
+            .map_err(|_| {
+                InstanceError::PermissionError(s!("Unable to copy input file into box directory"))
+            })?;
         Command::new("cp")
             .arg(self.bin_path.to_str().unwrap())
             .arg(self.box_path.to_str().unwrap())
             .output()
-            .map_err(|_| InstanceError::PermissionError(
-                String::from("Unable to copy user exec file into box directory")
-            ))?;
+            .map_err(|_| {
+                InstanceError::PermissionError(s!(
+                    "Unable to copy user exec file into box directory"
+                ))
+            })?;
         Command::new("cp")
             .arg(self.runner_path.to_str().unwrap())
             .arg(self.box_path.to_str().unwrap())
             .output()
-            .map_err(|_| InstanceError::PermissionError(
-                String::from("Unable to copy runner script into box directory")
-            ))?;
+            .map_err(|_| {
+                InstanceError::PermissionError(s!(
+                    "Unable to copy runner script into box directory"
+                ))
+            })?;
         Ok(())
     }
 
     pub fn run(&self) -> Result<InstanceResult, InstanceError> {
         let arg = self.get_arguments();
-        Err(InstanceError::PermissionError(String::new()))
+        Err(InstanceError::PermissionError(s!("")))
     }
 
     pub fn cleanup() -> Result<(), InstanceError> {
