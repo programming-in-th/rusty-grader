5b3294bc4db69a3fd6c40fec4552cc043016eaf1
Possawat Suksai <markkung08@gmail.com>
2021-09-15 02:00:48 +0000

    New setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo "Setting up isolate..."
+echo 0 > /proc/sys/kernel/randomize_va_space
+echo never > /sys/kernel/mm/transparent_hugepage/enabled
+echo never > /sys/kernel/mm/transparent_hugepage/defrag
+echo 0 > /sys/kernel/mm/transparent_hugepage/khugepaged/defrag
+
+apt install make gcc libcap-dev
+make isolate
+cp isolate /usr/local/bin/isolate
+cp default.cf /usr/local/etc/isolate
+
+echo "Setting up C++ compiler and Rust's cargo"
+apt install g++ cargo
