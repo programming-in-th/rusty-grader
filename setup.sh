245f5d1312bd8c822dda7f12bee7a13f365ed99e
Pongsaphol Pongsawakul <30414878+win11905@users.noreply.github.com>
2021-09-19 14:39:14 +0000

    Change setup.sh to absolute path

--- setup.sh
+++ setup.sh
@@ -4,35 +4,36 @@ green=$(tput setaf 2)
 blue=$(tput setaf 4)
 normal=$(tput sgr0)
 
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
 echo "${green}Cloning Submodule${normal}"
-git submodule update --init --recursive
+git -C ${SCRIPTPATH} submodule update --init --recursive
 
 echo "${green}Setting up isolate's package${normal}"
 apt install make gcc libcap-dev
 
-tput setaf 2; echo "Setting up isolate..."
+echo "Setting up isolate..."
 echo 0 > /proc/sys/kernel/randomize_va_space
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
 echo never > /sys/kernel/mm/transparent_hugepage/defrag
 echo 0 > /sys/kernel/mm/transparent_hugepage/khugepaged/defrag
 
-cd isolate
-make isolate
-cp isolate /usr/local/bin/isolate
-cp default.cf /usr/local/etc/isolate
-cd ..
+make -C ${SCRIPTPATH}/isolate isolate
+cp ${SCRIPTPATH}/isolate/isolate /usr/local/bin/isolate
+cp ${SCRIPTPATH}/isolate/default.cf /usr/local/etc/isolate
 
 echo "${green}Setting up C++ compiler and Rust's cargo${normal}"
 apt install g++ cargo
 
-echo "${green}Compiling checkers${normal}"
+echo "${green}Compiling checkers${normal}\n"
 
-mkdir -p example/scripts/checkers
+mkdir -p ${SCRIPTPATH}/example/scripts/checkers
 
-for file in testlib/checkers/*
+for file in ${SCRIPTPATH}/testlib/checkers/*
 do
   filename_ex=${file##*/}
   echo "${blue}Compiling ${filename_ex}${normal}"
   filename=${filename_ex%.*}
-  g++ -std=c++11 ${file} -O2 -o example/scripts/checkers/${filename} -I testlib/
+  g++ -std=c++11 ${file} -O2 -o ${SCRIPTPATH}/example/scripts/checkers/${filename} -I ${SCRIPTPATH}/testlib
 done
