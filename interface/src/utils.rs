18382e4b44fc67f7a5808c2ed9e0b9b6b94cf957
Pongsaphol Pongsawakul <donothavemoney@gmail.com>
2022-07-19 16:05:02 +0000

    fixed parsing code & add more test

--- interface/src/utils.rs
+++ interface/src/utils.rs
@@ -1,21 +1,22 @@
 use serde_json::Value;
 
 pub fn parse_code(code: &str) -> Vec<String> {
+    println!("{}", code);
+    let tmp = "{\"\\\\\\\",\\\"\\\\\"}";
+    println!("{}", tmp == code);
     let code = code.trim_end_matches("}").trim_start_matches("{");
     let mut ans = Vec::new();
     let mut start_string = false;
     let mut extra = false;
-    let mut pre = '\0';
+    let mut pre = ',';
     for ch in code.chars() {
         if ch == '"' && pre != '\\' {
             start_string = !start_string;
-        }
-        if pre == ',' && start_string == false {
+        } else if pre == ',' && start_string == false {
             start_string = true;
             extra = true;
             ans.push('"');
-        }
-        if ch == ',' && start_string == true && extra == true {
+        } else if ch == ',' && start_string == true && extra == true {
             start_string = false;
             extra = false;
             ans.push('"');
@@ -28,6 +29,7 @@ pub fn parse_code(code: &str) -> Vec<String> {
     }
     let ans: String = ans.iter().collect();
     let code = "[".to_string() + &ans + "]";
+    println!("{}", code);
     serde_json::from_str::<Value>(&code)
         .unwrap()
         .as_array()
@@ -46,5 +48,31 @@ mod tests {
         let code = "{\"hello , world\",quote,\"\\\",\\\"q\"}";
         let ans = parse_code(code);
         assert_eq!(ans, vec!["hello , world", "quote", "\",\"q"]);
+    }
+    #[test]
+    fn test_parse_one_elem() {
+        let code = "{print(5)}";
+        let ans = parse_code(code);
+        assert_eq!(ans, vec!["print(5)"]);
+    }
+    #[test]
+    fn test_parse_all_single() {
+        let code = "{print(5),print(6),print(7)}";
+        let ans = parse_code(code);
+        assert_eq!(ans, vec!["print(5)", "print(6)", "print(7)"]);
+    }
+
+    #[test]
+    fn test_parse_all_quote() {
+        let code = "{\"\\\\\\\\, \\\\\\\\\\\\,\"}";
+        let ans = parse_code(code);
+        assert_eq!(ans, vec!["\\\\, \\\\\\,"]);
+    }
+
+    #[test]
+    fn test_random_complicated_token() {
+        let code = "{\"\\\\\\\",\\\"\\\\\"}";
+        let ans = parse_code(code);
+        assert_eq!(ans, vec!["\\\",\"\\"]);
     }
 }
